aeb8801ae7305feba3d78eb94a75e8d0f7829283
tianxiang9376 <moxiang9376@foxmail.com>
2018-04-16 09:54:07 +0800

    JS2.1

--- task2/js/JS2-1-1.js
+++ task2/js/JS2-1-1.js
@@ -1,8 +1,9 @@
-function deal() {
+function setrole() {
     var n = parseInt(document.getElementById("num").value); //获取input的值,从字符串中提取整数
     var commoner;
     var killer;
     var people = new Array(n);
+    var str="";
     if (n > 18 || n < 4) { //判断input的值，当人数大于18或者小于4时，弹窗提示
         alert("请输入正确的玩家数量");
     }
@@ -11,30 +12,24 @@ function deal() {
         killer = Math.round(n / 4);
         commoner = n - killer;
         //给玩家分配角色
-        document.getElementById("killer").innerHTML = killer;
-        document.getElementById("commoner").innerHTML = commoner;
         for (var i = 0; i < people.length; i++) {
             if (i < killer) {
-                people[i] = "杀手"; //三分之一的角色分配为杀手
+                people[i] = '<li><span class="bot1"></span>' + '杀手' + '</li>'; //三分之一的角色分配为杀手
             } else {
-                people[i] = "平民";
+                people[i] = '<li><span class="bot2"></span>' + '平民' + '</li>';
             }
-            //测试demo
-            //document.write(i+1+"号玩家身份为"+people[i] + "</br>");
         }
     }
-
     for (var i = people.length; i--;) { //数组乱序,打乱身份的分配
         var j = Math.floor(Math.random() * (i + 1));
         var temp = people[i];
         people[i] = people[j];
         people[j] = temp;
     }
-    //测试demo
-    /*for (var i = 0; i < people.length; i++) {
-        document.write(i+1+"号玩家身份为"+people[i] + "</br>");
-    }*/
-
+    for (var i = 0; i < people.length; i++) {
+        str += people[i];
+    }
+    document.getElementById('role').innerHTML = str;
 }
 
 function change() {
